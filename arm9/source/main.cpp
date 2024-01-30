b999645e3e821f6abda1c540b0c3876631d110bc
Chloe Greene <thecatliferules@gmail.com>
2024-01-29 19:28:38 -0500

    This is so needlessly complicated

---
+++ arm9/source/main.cpp
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <time.h>
+#include <nds.h>
+#include <maxmod9.h>
+
+//compiler stuff and tricks
+#ifdef __clang__
+  #include <arm9/videoGL.h>
+  #include <keyboard.h>
+#endif
+
+
+int main(){
+
+	
+
+}
